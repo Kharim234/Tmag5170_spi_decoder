85a4678c84eab5509da5464bba9f06d9b8b8973e
Michal <malinowskimichal27@gmail.com>
2025-02-23 21:52:54 +0100

    move part of decoder into separate class

--- HighLevelAnalyzer.py
+++ HighLevelAnalyzer.py
@@ -20,64 +20,6 @@ TMAG5170_16_BIT_SPI_DATA_MASK = 0xFFFF
 
 TMAG5170_SINGLE_FRAME_BYTE_SIZE = 4
 
-def CONV_STATUS_DecodingFunction(data: int):
-    return "Not yet implemented"
-
-def X_CH_RESULT_DecodingFunction(data: int):
-    return f"X_CH_RESULT: {data}"
-
-def Y_CH_RESULT_DecodingFunction(data: int):
-    return f"Y_CH_RESULT: {data}"
-
-def Z_CH_RESULT_DecodingFunction(data: int):
-    return f"Z_CH_RESULT: {data}"
-
-def TEMP_RESULT_DecodingFunction(data: int):
-    return f"TEMP_RESULT: {data}"
-
-def AFE_STATUS_DecodingFunction(data: int):
-    return "Not yet implemented"
-
-def SYS_STATUS_DecodingFunction(data: int):
-    return "Not yet implemented"
-
-def ANGLE_RESULT_DecodingFunction(data: int):
-    return f"ANGLE_RESULT: {data}"
-
-def TEST_CONFIG_DecodingFunction(data: int):
-    return "Not yet implemented"
-
-def dummyDecodingFunction(data: int):
-    return "Not yet implemented"
-tmag5170_mapping_type = collections.namedtuple('tmag5170_mapping_type', ['Acronym', 'DecodingFunction'])
-
-Tmag5170_register_mapping = {
-    0x00: tmag5170_mapping_type("DEVICE_CONFIG"    ,    dummyDecodingFunction)              ,
-    0x01: tmag5170_mapping_type("SENSOR_CONFIG"    ,    dummyDecodingFunction)              ,
-    0x02: tmag5170_mapping_type("SYSTEM_CONFIG"    ,    dummyDecodingFunction)              ,
-    0x03: tmag5170_mapping_type("ALERT_CONFIG"     ,    dummyDecodingFunction)              ,
-    0x04: tmag5170_mapping_type("X_THRX_CONFIG"    ,    dummyDecodingFunction)              ,
-    0x05: tmag5170_mapping_type("Y_THRX_CONFIG"    ,    dummyDecodingFunction)              ,
-    0x06: tmag5170_mapping_type("Z_THRX_CONFIG"    ,    dummyDecodingFunction)              ,
-    0x07: tmag5170_mapping_type("T_THRX_CONFIG"    ,    dummyDecodingFunction)              ,
-    0x08: tmag5170_mapping_type("CONV_STATUS"      ,    CONV_STATUS_DecodingFunction)       ,
-    0x09: tmag5170_mapping_type("X_CH_RESULT"      ,    X_CH_RESULT_DecodingFunction)       ,
-    0x0A: tmag5170_mapping_type("Y_CH_RESULT"      ,    Y_CH_RESULT_DecodingFunction)       ,
-    0x0B: tmag5170_mapping_type("Z_CH_RESULT"      ,    Z_CH_RESULT_DecodingFunction)       ,
-    0x0C: tmag5170_mapping_type("TEMP_RESULT"      ,    TEMP_RESULT_DecodingFunction)       ,
-    0x0D: tmag5170_mapping_type("AFE_STATUS"       ,    AFE_STATUS_DecodingFunction)        ,
-    0x0E: tmag5170_mapping_type("SYS_STATUS"       ,    SYS_STATUS_DecodingFunction)        ,
-    0x0F: tmag5170_mapping_type("TEST_CONFIG"      ,    TEST_CONFIG_DecodingFunction)       ,
-    0x10: tmag5170_mapping_type("OSC_MONITOR"      ,    dummyDecodingFunction)              ,
-    0x11: tmag5170_mapping_type("MAG_GAIN_CONFIG"  ,    dummyDecodingFunction)              ,
-    0x12: tmag5170_mapping_type("MAG_OFFSET_CONFIG",    dummyDecodingFunction)              ,
-    0x13: tmag5170_mapping_type("ANGLE_RESULT"     ,    ANGLE_RESULT_DecodingFunction)      ,
-    0x14: tmag5170_mapping_type("MAGNITUDE_RESULT" ,    dummyDecodingFunction)     
-}
-
-def get_16_bit_spi_data_tmag5170 (value: int) -> int:
-    return get_masked_value(value, TMAG5170_16_BIT_SPI_DATA_POSITION, TMAG5170_16_BIT_SPI_DATA_MASK)
-
 def get_masked_value (value: int, position: int, mask: int) -> int:
     return ((value  >> position)& mask) 
 
@@ -87,39 +29,131 @@ def get_bit (value: int, i: int) -> int:
 def set_bit (bit_val: int, position: int) -> int:
     return (bit_val << position)
 
-def convert_tmag5170_bytes_to_int (data, start_position: int) -> int:
-    value = None
-    length = len(data)
-    if ((length - start_position) >= TMAG5170_SINGLE_FRAME_BYTE_SIZE):
-        print("data")
-        print(data)
+def uint16_to_int16 (in_value: int) -> int:
+    value = in_value.to_bytes(2, 'big')
+    int_val = int.from_bytes(value, 'big', signed = True)
+    return (int_val)
 
-        data_sliced = data[start_position:(start_position + TMAG5170_SINGLE_FRAME_BYTE_SIZE)]
-        print("data_sliced")
-        print(data_sliced)
-        data_joined = b''.join(data_sliced)
-        value = int.from_bytes(data_joined, 'big', signed = False) 
-    return value
+class tmga5170_frame_decoder:
+    def __init__(self):
+        self.__tmag5170_mapping_type = collections.namedtuple('__tmag5170_mapping_type', ['Acronym', 'DecodingFunction'])
+        self.__Tmag5170_register_mapping = {
+            0x00: self.__tmag5170_mapping_type("DEVICE_CONFIG"    ,    self.__dummyDecodingFunction)              ,
+            0x01: self.__tmag5170_mapping_type("SENSOR_CONFIG"    ,    self.__dummyDecodingFunction)              ,
+            0x02: self.__tmag5170_mapping_type("SYSTEM_CONFIG"    ,    self.__dummyDecodingFunction)              ,
+            0x03: self.__tmag5170_mapping_type("ALERT_CONFIG"     ,    self.__dummyDecodingFunction)              ,
+            0x04: self.__tmag5170_mapping_type("X_THRX_CONFIG"    ,    self.__dummyDecodingFunction)              ,
+            0x05: self.__tmag5170_mapping_type("Y_THRX_CONFIG"    ,    self.__dummyDecodingFunction)              ,
+            0x06: self.__tmag5170_mapping_type("Z_THRX_CONFIG"    ,    self.__dummyDecodingFunction)              ,
+            0x07: self.__tmag5170_mapping_type("T_THRX_CONFIG"    ,    self.__dummyDecodingFunction)              ,
+            0x08: self.__tmag5170_mapping_type("CONV_STATUS"      ,    self.__CONV_STATUS_DecodingFunction)       ,
+            0x09: self.__tmag5170_mapping_type("X_CH_RESULT"      ,    self.__X_CH_RESULT_DecodingFunction)       ,
+            0x0A: self.__tmag5170_mapping_type("Y_CH_RESULT"      ,    self.__Y_CH_RESULT_DecodingFunction)       ,
+            0x0B: self.__tmag5170_mapping_type("Z_CH_RESULT"      ,    self.__Z_CH_RESULT_DecodingFunction)       ,
+            0x0C: self.__tmag5170_mapping_type("TEMP_RESULT"      ,    self.__TEMP_RESULT_DecodingFunction)       ,
+            0x0D: self.__tmag5170_mapping_type("AFE_STATUS"       ,    self.__AFE_STATUS_DecodingFunction)        ,
+            0x0E: self.__tmag5170_mapping_type("SYS_STATUS"       ,    self.__SYS_STATUS_DecodingFunction)        ,
+            0x0F: self.__tmag5170_mapping_type("TEST_CONFIG"      ,    self.__TEST_CONFIG_DecodingFunction)       ,
+            0x10: self.__tmag5170_mapping_type("OSC_MONITOR"      ,    self.__dummyDecodingFunction)              ,
+            0x11: self.__tmag5170_mapping_type("MAG_GAIN_CONFIG"  ,    self.__dummyDecodingFunction)              ,
+            0x12: self.__tmag5170_mapping_type("MAG_OFFSET_CONFIG",    self.__dummyDecodingFunction)              ,
+            0x13: self.__tmag5170_mapping_type("ANGLE_RESULT"     ,    self.__ANGLE_RESULT_DecodingFunction)      ,
+            0x14: self.__tmag5170_mapping_type("MAGNITUDE_RESULT" ,    self.__dummyDecodingFunction)     
+        }
 
-def calculate_tmag5170_crc (data: int):
-    crc_calculated = None
-    crc_from_bus = None
-    if (data != None):
-        crc_from_bus = data & 0x0F
-        padded_frame = data & 0xFFFFFFF0
-        frame_crc = 0x0F
-        for i in reversed(range(32)):
-            inv = get_bit(padded_frame, i) ^ get_bit(frame_crc, 3)
-            frame_crc_3bit = get_bit(frame_crc, 2)
-            frame_crc_2bit = get_bit(frame_crc, 1)
-            frame_crc_1bit = get_bit(frame_crc, 0) ^ inv
-            frame_crc_0bit = inv
-            frame_crc = (set_bit(frame_crc_3bit, 3) |\
-                        set_bit(frame_crc_2bit, 2) |\
-                        set_bit(frame_crc_1bit, 1) |\
-                        set_bit(frame_crc_0bit, 0)) & 0x0F
-        crc_calculated = frame_crc
-    return crc_calculated, crc_from_bus
+    def __CONV_STATUS_DecodingFunction(self, data: int):
+        return "Not yet implemented"
+
+    def __X_CH_RESULT_DecodingFunction(self, data: int):
+        int_val = uint16_to_int16(data)
+        return f"[15-0] X_CH_RESULT: {int_val}"
+
+    def __Y_CH_RESULT_DecodingFunction(self, data: int):
+        int_val = uint16_to_int16(data)
+        return f"[15-0] Y_CH_RESULT: {int_val}"
+
+    def __Z_CH_RESULT_DecodingFunction(self, data: int):
+        int_val = uint16_to_int16(data)
+        return f"[15-0] Z_CH_RESULT: {int_val}"
+
+    def __TEMP_RESULT_DecodingFunction(self, data: int):
+        return f"[15-0] TEMP_RESULT: {data}"
+
+    def __AFE_STATUS_DecodingFunction(self, data: int):
+        return "Not yet implemented"
+
+    def __SYS_STATUS_DecodingFunction(self, data: int):
+        return "Not yet implemented"
+
+    def __ANGLE_RESULT_DecodingFunction(self, data: int):
+        return f"[15-0] ANGLE_RESULT: {data}"
+
+
+    def __TEST_CONFIG_DecodingFunction(self, data: int):
+        return "Not yet implemented"
+
+    def __dummyDecodingFunction(self, data: int):
+        return "Not yet implemented"
+    
+    def get_16_bit_spi_data_tmag5170 (self, value: int) -> int:
+        return get_masked_value(value, TMAG5170_16_BIT_SPI_DATA_POSITION, TMAG5170_16_BIT_SPI_DATA_MASK)
+        
+    def get_register_acronym(self, register_index: int):
+        return self.__Tmag5170_register_mapping[register_index].Acronym
+    
+    def get_register_decoded_description(self, register_index: int, data_32_bit_spi: int):
+        data_16_bit_spi = self.get_16_bit_spi_data_tmag5170(data_32_bit_spi)
+        return self.__Tmag5170_register_mapping[register_index].DecodingFunction(data_16_bit_spi)
+
+    def get_register_index_from_tmag5170_frame (self, mosi_value: int) -> int:
+        return get_masked_value(mosi_value, REGISTER_ADDR_POSITION, REGISTER_ADDR_MASK)
+        
+    def convert_tmag5170_bytes_to_int (self, data, start_position: int) -> int:
+        value = None
+        length = len(data)
+        if ((length - start_position) >= TMAG5170_SINGLE_FRAME_BYTE_SIZE):
+            print("data")
+            print(data)
+
+            data_sliced = data[start_position:(start_position + TMAG5170_SINGLE_FRAME_BYTE_SIZE)]
+            print("data_sliced")
+            print(data_sliced)
+            data_joined = b''.join(data_sliced)
+            value = int.from_bytes(data_joined, 'big', signed = False) 
+        return value
+
+    def calculate_tmag5170_crc (self, data: int):
+        crc_calculated = None
+        crc_from_bus = None
+        crc_status = None
+
+        if (data != None):
+            crc_from_bus = data & 0x0F
+            padded_frame = data & 0xFFFFFFF0
+            frame_crc = 0x0F
+            for i in reversed(range(32)):
+                inv = get_bit(padded_frame, i) ^ get_bit(frame_crc, 3)
+                frame_crc_3bit = get_bit(frame_crc, 2)
+                frame_crc_2bit = get_bit(frame_crc, 1)
+                frame_crc_1bit = get_bit(frame_crc, 0) ^ inv
+                frame_crc_0bit = inv
+                frame_crc = (set_bit(frame_crc_3bit, 3) |\
+                            set_bit(frame_crc_2bit, 2) |\
+                            set_bit(frame_crc_1bit, 1) |\
+                            set_bit(frame_crc_0bit, 0)) & 0x0F
+            crc_calculated = frame_crc
+            if crc_calculated == crc_from_bus:
+                crc_status = CRC_OK_TOKEN
+            else:
+                crc_status = CRC_ERROR_TOKEN
+        return crc_calculated, crc_from_bus, crc_status
+    
+
+
+
+
+
+
 
 # High level analyzers must subclass the HighLevelAnalyzer class.
 class Hla(HighLevelAnalyzer):
@@ -151,6 +185,8 @@ class Hla(HighLevelAnalyzer):
             {{data.crc_miso_correct}}'
         }
     }
+    
+    decoder = tmga5170_frame_decoder()
 
     def __init__(self):
         '''
@@ -187,22 +223,16 @@ class Hla(HighLevelAnalyzer):
             miso_crc_calculated = 0
             if(len(self.frame_data_MOSI) == TMAG5170_SINGLE_FRAME_BYTE_SIZE):
                 MOSI  = "0x"
-                mosi_value = convert_tmag5170_bytes_to_int(self.frame_data_MOSI, 0)
-                mosi_crc_calculated, mosi_crc_from_bus = calculate_tmag5170_crc(mosi_value)
-                register_address = get_masked_value(mosi_value, REGISTER_ADDR_POSITION, REGISTER_ADDR_MASK)
-                register_name = Tmag5170_register_mapping[register_address].Acronym
-                
-                if mosi_crc_calculated == mosi_crc_from_bus and mosi_crc_calculated != None:
-                    crc_mosi_correct = CRC_OK_TOKEN
-                else:
-                    crc_mosi_correct = CRC_ERROR_TOKEN
+                mosi_value = self.decoder.convert_tmag5170_bytes_to_int(self.frame_data_MOSI, 0)
+                mosi_crc_calculated, mosi_crc_from_bus, crc_mosi_correct = self.decoder.calculate_tmag5170_crc(mosi_value)
+                register_address = self.decoder.get_register_index_from_tmag5170_frame(mosi_value)
+                register_name = self.decoder.get_register_acronym(register_address)
 
                 if get_bit(mosi_value, READ_WRITE_BIT_POSITION) == 1:
                     read_write = READ_REGISTER_TOKEN
                 else:
                     read_write = WRITE_REGISTER_TOKEN
-                    tmag5170_16_bit_spi_data = get_16_bit_spi_data_tmag5170(mosi_value)
-                    register_decoding = Tmag5170_register_mapping[register_address].DecodingFunction(tmag5170_16_bit_spi_data)
+                    register_decoding = self.decoder.get_register_decoded_description(register_address, mosi_value)
 
                 for i in self.frame_data_MOSI:
                     MOSI += (i.hex()).upper()
@@ -211,16 +241,11 @@ class Hla(HighLevelAnalyzer):
             
             if(len(self.frame_data_MISO) == TMAG5170_SINGLE_FRAME_BYTE_SIZE):
                 MISO  = "0x"
-                miso_value = convert_tmag5170_bytes_to_int(self.frame_data_MISO, 0)
-                miso_crc_calculated, miso_crc_from_bus = calculate_tmag5170_crc(miso_value)
-                if miso_crc_calculated == miso_crc_from_bus and miso_crc_calculated != None:
-                    crc_miso_correct = CRC_OK_TOKEN
-                else:
-                    crc_miso_correct = CRC_ERROR_TOKEN
+                miso_value = self.decoder.convert_tmag5170_bytes_to_int(self.frame_data_MISO, 0)
+                miso_crc_calculated, miso_crc_from_bus, crc_miso_correct = self.decoder.calculate_tmag5170_crc(miso_value)
 
                 if read_write == READ_REGISTER_TOKEN:
-                    tmag5170_16_bit_spi_data = get_16_bit_spi_data_tmag5170(miso_value)
-                    register_decoding = Tmag5170_register_mapping[register_address].DecodingFunction(tmag5170_16_bit_spi_data)
+                    register_decoding = self.decoder.get_register_decoded_description(register_address, miso_value)
                 
                 for i in self.frame_data_MISO:
                     MISO += (i.hex()).upper()

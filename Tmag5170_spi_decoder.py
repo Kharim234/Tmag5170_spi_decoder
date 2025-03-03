452705851e8fe0c16f5b1632c206555a76104a23
Michal <malinowskimichal27@gmail.com>
2025-03-03 22:49:19 +0100

    Fix formatting

--- Tmag5170_spi_decoder.py
+++ Tmag5170_spi_decoder.py
@@ -607,12 +607,15 @@ class Hla(HighLevelAnalyzer):
             'format': \
             'MOSI:{{data.mosi_frame}}, \
             crc_mosi_expected: {{data.mosi_crc_calculated}},\
+            crc_mosi_from_bus: {{data.mosi_crc_from_bus}},\
             {{data.crc_mosi_correct}}, \
-            R/W:{{data.read_write}}, \
-            RegAddr:{{data.register_address}} - {{data.register_name}}, \
             \nMISO:{{data.miso_frame}}, \
             crc_miso_expected: {{data.miso_crc_calculated}}, \
+            crc_miso_from_bus: {{data.miso_crc_from_bus}}, \
             {{data.crc_miso_correct}}, \
+            R/W:{{data.read_write}}, \
+            RegAddr:{{data.register_address}} - {{data.register_name}}, \
+            reg_val:{{data.register_value}}, \
             decoded_reg_val:{{data.register_decoding}}, \
             FrameCnt_debug:{{data.FrameCnt_debug}}'\
         },
@@ -620,14 +623,17 @@ class Hla(HighLevelAnalyzer):
             'format': \
             'MOSI:{{data.mosi_frame}}, \
             crc_mosi_expected: {{data.mosi_crc_calculated}},\
+            crc_mosi_from_bus: {{data.mosi_crc_from_bus}},\
             {{data.crc_mosi_correct}}, \
             \nMISO:{{data.miso_frame}}, \
             crc_miso_expected: {{data.miso_crc_calculated}}, \
+            crc_miso_from_bus: {{data.miso_crc_from_bus}}, \
             {{data.crc_miso_correct}}, \
             ch1_value:{{data.ch1_value}}, \
             ch2_value:{{data.ch2_value}}, \
             R/W:{{data.read_write}}, \
             RegAddr:{{data.register_address}} - {{data.register_name}}, \
+            reg_val:{{data.register_value}}, \
             decoded_reg_val:{{data.register_decoding}}, \
             FrameCnt_debug:{{data.FrameCnt_debug}}'\
         }
